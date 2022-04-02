b6d4353ae37f6816d2c8ec5bda4eb5d12829395b
Kevin Lee <kmlee1127@gmail.com>
2022-04-02 14:23:22 -0700

    hide location console message

--- client/src/views/Dash/components/Weather/index.js
+++ client/src/views/Dash/components/Weather/index.js
@@ -44,7 +44,7 @@ export default class Weather extends Component {
     if (!navigator.geolocation) {
       // console.log("Geolocation is not supported by your browser");
     } else {
-      console.log("Locating...");
+      //console.log("Locating...");
       navigator.geolocation.getCurrentPosition(
         (position) => {
           // console.log("Found");
