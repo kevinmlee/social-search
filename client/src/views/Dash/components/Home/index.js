f5edb10359c15a17326bea1b31b20f8c6a86d688
Kevin Lee <kmlee1127@gmail.com>
2022-05-16 20:03:03 -0700

    Update index.js

--- client/src/views/Dash/components/Home/index.js
+++ client/src/views/Dash/components/Home/index.js
@@ -63,6 +63,17 @@ export default class Home extends Component {
       .replaceAll("&gt;", ">");
   };
 
+  isInViewport = (element) => {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
   getVideo = (post) => {
     if ("secure_media" in post.data) {
       if (post.data.secure_media) {
