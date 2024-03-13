f3c0ed547e37561dde5f418fd59bdc6b44587612
Kevin Lee <kmlee1127@gmail.com>
2024-03-13 19:52:55 +0000

    update getSubredditPosts

--- client/netlify/functions/reddit/getSubredditPosts.js
+++ client/netlify/functions/reddit/getSubredditPosts.js
@@ -4,16 +4,11 @@ const endpoint = 'https://www.reddit.com'
 
 exports.handler = async (event, context) => {
   const { subreddit, filter, limit } = JSON.parse(event.body)
-
-  const url = `${endpoint}/r/${subreddit}/${filter}.json?limit=${limit}`;
-
-  // serverless API call
-  const result = await fetch(url)
-    .then((response) => response.json())
-    .then((data) => data);
+  const url = `${endpoint}/r/${subreddit}/${filter}.json?limit=${limit}`
+  const result = await fetch(url).then((response) => response.json())
 
   return {
     statusCode: 200,
-    body: JSON.stringify(result),
-  };
+    body: JSON.stringify(result)
+  }
 }
