d715cc6bd90a24b3bc7706cc9e577f4b14d06d30
Kevin Lee <kmlee1127@gmail.com>
2024-03-20 09:30:10 -0700

    refactor: trends (#24)
    * refactor trends
    
    * update trends
    
    * test
    
    * test

--- client/src/views/Trends/Trends.js
+++ client/src/views/Trends/Trends.js
@@ -1,54 +1,169 @@
-import React, { Component } from "react";
-import moment from "moment";
+import React, { useCallback, useEffect, useRef, useState } from "react"
+import { useParams } from "react-router-dom"
+// import moment from "moment"
+// import axios from "axios"
+
 import { Grid, Box, Typography, Radio } from "@mui/material";
-import TuneRoundedIcon from "@mui/icons-material/TuneRounded";
+import TuneRoundedIcon from "@mui/icons-material/TuneRounded"
+//import Chart from "chart.js/auto"
+import { Line } from "react-chartjs-2"
+//import "chartjs-plugin-trendline"
+import "./Trends.css"
 
-//import Chart from "chart.js/auto";
-import { Line } from "react-chartjs-2";
-//import "chartjs-plugin-trendline";
 
-import axios from "axios";
+export default function Trends() {
+  const { query } = useParams()
+  const ref = useRef(null)
+  const [filterToggle, setFilterToggle] = useState(false)
+  const [trendingTopics, setTrendingTopics] = useState([])
+  const [trendingQueries, setTrendingQueries] = useState([])
+  const [iotData, setIotData] = useState({})
+  // const [twelveMonths, setTwelveMonths] = useState(false)
+  // const [sixMonths, setSixMonths] = useState(false)
+  // const [full, setFull] = useState(true)
+  const twelveMonths = false
+  const sixMonths = false
+  const full = true
 
-import "./Trends.css";
+  const getInterestOverTime = useCallback(async () => {
+    await fetch(`/.netlify/functions/trends-interestOverTime`, {
+      method: "POST",
+      body: JSON.stringify({
+        searchQuery: query,
+        //startTime: new Date("2010-01-01").toISOString(),
+        //endTime: new Date(Date.now()),
+        //time: "today 12-m",
+        granularTimeResolution: true
+      }),
+    }).then(response => {
+      console.log('getInterestOverTime', response)
 
-export default class Trends extends Component {
-  constructor(props) {
-    super(props);
+      /*
+      const timelineData = JSON.parse(response.data).default.timelineData
+      //console.log(timelineData);
 
-    this.state = {
-      filterToggle: false,
+      let borderColor = ""
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches)
+        borderColor = "rgba(255, 255, 255, 1)"
+      else if (window.matchMedia("(prefers-color-scheme: light)").matches)
+        borderColor = "rgba(0, 0, 0, 1)"
 
-      trendingTopics: [],
-      trendingQueries: [],
-      iotData: {},
+      let iotData = {
+        labels: generateLabels(timelineData),
+        datasets: [
+          {
+            label: "Peak popularity",
+            data: generateData(timelineData),
+            //backgroundColor: "rgba(255, 255, 255, 1)",
+            borderColor: borderColor,
+            //backgroundColor: gradient,
+            //pointBackgroundColor: "white",
+            //borderWidth: 1,
+          }
+        ]
+      }
 
-      full: true,
-      twelveMonths: false,
-      sixMonths: false,
-    };
+      setIotData(iotData)
+      */
 
-    // this.wrapperRef = React.createRef();
-    //this.handleClickOutside = this.handleClickOutside.bind(this);
+      setIotData({})
+    })
+  }, [query])
+
+  const getRelatedTopics = useCallback(async () => {
+    await fetch(`/.netlify/functions/trends-relatedTopics`, {
+      method: "POST",
+      body: JSON.stringify({
+        searchQuery: query
+      }),
+    }).then(response => {
+      console.log('relatedTopics', response)
+
+      /*
+      // if response is as expected
+      if ("default" in JSON.parse(response.data)) {
+        const rankedList = JSON.parse(response.data).default.rankedList
+        setTrendingTopics(rankedList[0].rankedKeyword)
+      }
+      */
+
+      setTrendingTopics([])
+    })
+
+    /*
+    return await axios
+      .put("/google/relatedTopics", {
+        //searchQuery: "valkyrae",
+        searchQuery: query
+      })
+      .then(
+        response => {
+          // if response is as expected
+          if ("default" in JSON.parse(response.data)) {
+            const rankedList = JSON.parse(response.data).default.rankedList
+            setTrendingTopics(rankedList[0].rankedKeyword)
+          }
+        },
+        error => console.log(error)
+      )
+      */
+  }, [query])
+
+  const getRelatedQueries = useCallback(async () => {
+    await fetch(`/.netlify/functions/trends-relatedQueries`, {
+      method: "POST",
+      body: JSON.stringify({
+        searchQuery: query
+      }),
+    }).then(response => {
+      console.log('relatedQueries', response)
+
+      /*
+      // if response is as expected
+      if ("default" in JSON.parse(response.data)) {
+        const rankedList = JSON.parse(response.data).default.rankedList
+        setTrendingQueries(rankedList[0].rankedKeyword)
+      }
+      */
+
+      setTrendingQueries([])
+    })
+
+    /*
+    return await axios
+      .put("/google/relatedQueries", {
+        //searchQuery: "valkyrae",
+        searchQuery: query,
+      })
+      .then(
+        response => {
+          // if response is as expected
+          if ("default" in JSON.parse(response.data)) {
+            const rankedList = JSON.parse(response.data).default.rankedList
+            setTrendingQueries(rankedList[0].rankedKeyword)
+          }
+        },
+        error => console.log(error)
+      )
+      */
+  }, [query])
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside)
+
+    getInterestOverTime()
+    getRelatedTopics()
+    getRelatedQueries()
+
+    return () => document.removeEventListener("mousedown", handleClickOutside)
+  }, [getInterestOverTime, getRelatedTopics, getRelatedQueries])
+
+  const handleClickOutside = (event) => {
+    if (ref && !ref.current.contains(event.target)) setFilterToggle(false)
   }
 
-  componentDidMount = () => {
-    document.addEventListener("mousedown", this.handleClickOutside);
-
-    this.getInterestOverTime();
-    this.getRelatedTopics();
-    this.getRelatedQueries();
-  };
-
-  componentWillUnmount = () => {
-    document.removeEventListener("mousedown", this.handleClickOutside);
-  };
-
-  handleClickOutside = (event) => {
-    if (this.wrapperRef && !this.wrapperRef.current.contains(event.target))
-      this.setState({ filterToggle: false });
-  };
-
-  changeTab = (event) => {
+  const changeTab = (event) => {
+    /*
     const tabs = ["full, twelveMonths, sixMonths"];
     const selectedTab = event.target.getAttribute("data-tab");
 
@@ -57,280 +172,161 @@ export default class Trends extends Component {
       else this.setState({ [tab]: false });
     });
 
-    this.setState({ filterToggle: false });
+    setFilterToggle(false)
     //this.iotFilter(selectedTab);
-  };
+    */
+  }
 
-  toggle = async (state) => {
-    await this.setState({ [state]: !this.state[state] });
-  };
+  /*
+ const iotFilter = async (filter) => {
+    const currentDay = moment(new Date(Date.now())).format("MMM D YYYY")
 
-  iotFilter = async (filter) => {
-    const currentDay = moment(new Date(Date.now())).format("MMM D YYYY");
+    let newIotData = iotData
+    let newIotDataset = []
 
-    let iotData = this.state.iotData;
-    let newIotDataset = [];
-
-    let filteredLabels = this.state.iotData.labels.filter((item, index) => {
+    let filteredLabels = iotData.labels.filter((item, index) => {
       if (moment(currentDay).diff(moment(item), "months", true) < 12)
-        newIotDataset.push(iotData.datasets[0].data[index]);
+        newIotDataset.push(iotData.datasets[0].data[index])
 
-      return moment(currentDay).diff(moment(item), "months", true) < 12;
-    });
+      return moment(currentDay).diff(moment(item), "months", true) < 12
+    })
 
-    iotData.labels = filteredLabels;
-    iotData.datasets[0].data = newIotDataset;
+    newIotData.labels = filteredLabels
+    newIotData.datasets[0].data = newIotDataset
 
     //console.log("newIotData", iotData);
-    await this.setState({ iotData });
+    setIotData(newIotData)
   };
+  */
 
-  filters = () => {
+  const filters = () => {
     return (
       <Box className="filter">
         <div
           className="active-display"
-          onClick={() => this.toggle("filterToggle")}
+          // onClick={() => this.toggle("filterToggle")}
         >
           <span className="active-filter">Filter</span>
           <TuneRoundedIcon />
         </div>
-        <ul
-          className={"filter-options " + (this.state.filterToggle && "active")}
-          ref={this.wrapperRef}
-        >
+        <ul className={"filter-options " + (filterToggle && "active")} ref={ref} >
           <li
-            className={this.state.full ? "active" : ""}
-            onClick={this.changeTab}
+            className={full ? "active" : ""}
+            onClick={() => changeTab}
             data-tab="full"
           >
             2004 - Present
-            <Radio checked={this.state.full && "checked"} size="small" />
+            <Radio checked={full && "checked"} size="small" />
           </li>
           <li
-            className={this.state.twelveMonths ? "active" : ""}
-            onClick={this.changeTab}
+            className={twelveMonths ? "active" : ""}
+            onClick={() => changeTab}
             data-tab="twelveMonths"
           >
             Past 12 months
-            <Radio
-              checked={this.state.twelveMonths && "checked"}
-              size="small"
-            />
+            <Radio checked={twelveMonths && "checked"} size="small" />
           </li>
           <li
-            className={this.state.sixMonths ? "active" : ""}
-            onClick={this.changeTab}
+            className={sixMonths ? "active" : ""}
+            onClick={() => changeTab}
             data-tab="sixMonths"
           >
             Past 6 months
-            <Radio checked={this.state.sixMonths && "checked"} size="small" />
+            <Radio checked={sixMonths && "checked"} size="small" />
           </li>
         </ul>
       </Box>
-    );
-  };
+    )
+  }
 
-  interestOverTime = () => {
+  const interestOverTime = () => {
     let options = {
       plugins: {
         legend: {
-          display: false,
-        },
+          display: false
+        }
       },
       elements: {
         point: {
-          radius: 0,
-        },
+          radius: 0
+        }
       },
       scales: {
         y: {
-          beginAtZero: true,
-        },
-      },
-    };
+          beginAtZero: true
+        }
+      }
+    }
 
     return (
-      <Box
-        className="interest-over-time"
-        sx={{ paddingTop: 4, paddingBottom: 2 }}
-      >
+      <Box className="interest-over-time" sx={{ paddingTop: 4, paddingBottom: 2 }}>
         <div className="card">
-          <Typography variant="h5" sx={{ paddingBottom: 2 }}>
-            Interest over time
-          </Typography>
-          <Line id="iot" data={this.state.iotData} options={options} />
+          <Typography variant="h5" sx={{ paddingBottom: 2 }}>Interest over time</Typography>
+          <Line id="iot" data={iotData} options={options} />
         </div>
       </Box>
     );
-  };
-
-  getInterestOverTime = async (e) => {
-    return await axios
-      .put("/google/interestOverTime", {
-        //searchQuery: "valkyrae",
-        searchQuery: this.props.state.previousSearchQuery,
-        //startTime: new Date("2010-01-01").toISOString(),
-        //endTime: new Date(Date.now()),
-        //time: "today 12-m",
-        granularTimeResolution: true,
-      })
-      .then(
-        async (response) => {
-          const timelineData = JSON.parse(response.data).default.timelineData;
-          //console.log(timelineData);
-
-          let borderColor = "";
-          if (window.matchMedia("(prefers-color-scheme: dark)").matches)
-            borderColor = "rgba(255, 255, 255, 1)";
-          else if (window.matchMedia("(prefers-color-scheme: light)").matches)
-            borderColor = "rgba(0, 0, 0, 1)";
-
-          let iotData = {
-            labels: await this.generateLabels(timelineData),
-            datasets: [
-              {
-                label: "Peak popularity",
-                data: await this.generateData(timelineData),
-                //backgroundColor: "rgba(255, 255, 255, 1)",
-                borderColor: borderColor,
-                //backgroundColor: gradient,
-                //pointBackgroundColor: "white",
-                //borderWidth: 1,
-              },
-            ],
-          };
-
-          await this.setState({ iotData });
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-  };
-
-  getRelatedTopics = async (e) => {
-    return await axios
-      .put("/google/relatedTopics", {
-        //searchQuery: "valkyrae",
-        searchQuery: this.props.state.previousSearchQuery,
-      })
-      .then(
-        (response) => {
-          // if response is as expected
-          if ("default" in JSON.parse(response.data)) {
-            const rankedList = JSON.parse(response.data).default.rankedList;
-            this.setState({ trendingTopics: rankedList[0].rankedKeyword });
-          }
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-  };
-
-  getRelatedQueries = async (e) => {
-    return await axios
-      .put("/google/relatedQueries", {
-        //searchQuery: "valkyrae",
-        searchQuery: this.props.state.previousSearchQuery,
-      })
-      .then(
-        (response) => {
-          // if response is as expected
-          if ("default" in JSON.parse(response.data)) {
-            const rankedList = JSON.parse(response.data).default.rankedList;
-            this.setState({ trendingQueries: rankedList[0].rankedKeyword });
-          }
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-  };
-
-  generateLabels = (data) => {
-    let labels = [];
-
-    data.forEach((item) => {
-      labels.push(item.formattedAxisTime);
-    });
-
-    return labels;
-  };
-
-  generateData = (data) => {
-    let values = [];
-
-    data.forEach((item) => {
-      values.push(item.value[0]);
-    });
-
-    return values;
-  };
-
-  render() {
-    return (
-      <Box sx={{ paddingTop: 4, paddingBottom: 4 }}>
-        {/*this.filters()*/}
-
-        {"datasets" in this.state.iotData && this.interestOverTime()}
-
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <Box className="trending-topics card">
-              <Typography variant="h5">Related Topics</Typography>
-
-              <ul>
-                {this.state.trendingTopics.slice(0, 10).map((item, index) => {
-                  return (
-                    <li key={index}>
-                      <Grid container spacing={2} sx={{ alignItems: "center" }}>
-                        <Grid item xs={1}>
-                          {index + 1}
-                        </Grid>
-                        <Grid item xs={9}>
-                          {item.topic.title} ({item.topic.type})
-                        </Grid>
-                        <Grid
-                          item
-                          xs={2}
-                          sx={{ textAlign: "right" }}
-                        >{`${item.value}%`}</Grid>
-                      </Grid>
-                    </li>
-                  );
-                })}
-              </ul>
-            </Box>
-          </Grid>
-          <Grid item xs={6}>
-            <Box className="trending-topics card">
-              <Typography variant="h5">Related Queries</Typography>
-
-              <ul>
-                {this.state.trendingQueries.slice(0, 10).map((item, index) => {
-                  return (
-                    <li key={index}>
-                      <Grid container spacing={2} sx={{ alignItems: "center" }}>
-                        <Grid item xs={1}>
-                          {index + 1}
-                        </Grid>
-                        <Grid item xs={9}>
-                          {item.query}
-                        </Grid>
-                        <Grid item xs={2} sx={{ textAlign: "right" }}>
-                          {`${item.value}%`}
-                        </Grid>
-                      </Grid>
-                    </li>
-                  );
-                })}
-              </ul>
-            </Box>
-          </Grid>
-        </Grid>
-      </Box>
-    );
   }
+
+  /*
+  const generateLabels = data => {
+    let labels = []
+    data.forEach(item => labels.push(item.formattedAxisTime))
+    return labels
+  }
+
+  const generateData = data => {
+    let values = []
+    data.forEach(item => values.push(item.value[0]))
+    return values
+  }
+  */
+
+  return (
+    <Box sx={{ paddingTop: 4, paddingBottom: 4 }}>
+      {filters()}
+      {"datasets" in iotData && interestOverTime()}
+
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Box className="trending-topics card">
+            <Typography variant="h5">Related Topics</Typography>
+
+            <ul>
+              {trendingTopics.slice(0, 10).map((item, index) => {
+                return (
+                  <li key={index}>
+                    <Grid container spacing={2} sx={{ alignItems: "center" }}>
+                      <Grid item xs={1}>{index + 1}</Grid>
+                      <Grid item xs={9}>{item.topic.title} ({item.topic.type})</Grid>
+                      <Grid item xs={2} sx={{ textAlign: "right" }}>{`${item.value}%`}</Grid>
+                    </Grid>
+                  </li>
+                )
+              })}
+            </ul>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <Box className="trending-topics card">
+            <Typography variant="h5">Related Queries</Typography>
+
+            <ul>
+              {trendingQueries.slice(0, 10).map((item, index) => {
+                return (
+                  <li key={index}>
+                    <Grid container spacing={2} sx={{ alignItems: "center" }}>
+                      <Grid item xs={1}>{index + 1}</Grid>
+                      <Grid item xs={9}>{item.query}</Grid>
+                      <Grid item xs={2} sx={{ textAlign: "right" }}>{`${item.value}%`}</Grid>
+                    </Grid>
+                  </li>
+                )
+              })}
+            </ul>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  )
 }
