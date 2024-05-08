59592e384eb126d273035cb00be11569fb9ae6bf
Kevin Lee <kmlee1127@gmail.com>
2024-05-08 07:08:00 +0000

    clean up setup file

--- client/jest.setup.js
+++ client/jest.setup.js
@@ -1,16 +1,9 @@
 import { TextEncoder, TextDecoder } from 'node:util'
 
-import fetch from 'node-fetch'
 
 // @see https://github.com/nock/nock/issues/2397
 global.TextEncoder = TextEncoder
 global.TextDecoder = TextDecoder
-global.fetch = fetch
-global.__incrementalCache = {
-  requestHeaders: {
-    'user-agent': ''
-  }
-}
 
 window.URL.createObjectURL = jest.fn()
 window.matchMedia = jest.fn().mockImplementation(() => ({
