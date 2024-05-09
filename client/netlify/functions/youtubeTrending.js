bd6625f466686e192e332fb6fe3d9934aac97977
Kevin Lee <kmlee1127@gmail.com>
2024-05-09 07:35:36 -0700

    add getLocation and update youtubeTrending (#78)

--- client/netlify/functions/youtubeTrending.js
+++ client/netlify/functions/youtubeTrending.js
@@ -3,9 +3,9 @@ const fetch = require("node-fetch");
 const endpoint = "https://www.googleapis.com/youtube";
 
 exports.handler = async (event, context) => {
-  // const { searchQuery, order } = JSON.parse(event.body);
-  // add "regionCode" for regionalized results: https://developers.google.com/youtube/v3/docs/videos/list
-  const url = `${endpoint}/v3/videos?part=snippet&chart=mostPopular&maxResults=15&key=${process.env.YOUTUBE_API_KEY}`;
+  const { countryCode } = JSON.parse(event.body);
+  const url = `${endpoint}/v3/videos?part=snippet&chart=mostPopular&maxResults=15&key=${process.env.YOUTUBE_API_KEY}` 
+    + (countryCode ? `&regionCode=${countryCode}` : '');
 
   // serverless API call
   const result = await fetch(url)
