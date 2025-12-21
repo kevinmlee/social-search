64da924b302fe86ba143aa1c77d8b6f4ed71ca43
Kevin Lee <kmlee1127@gmail.com>
2025-12-21 13:51:00 -0800

    make Home a server component

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,8 +1,7 @@
-'use client'
-
-import React, { useCallback, useEffect, useState } from "react"
+import React from "react"
 import Post from "./Post"
 import { ScrollToTopOnLoad } from "@/components"
+import TopicsBar from "./components/TopicsBar"
 
 const TOPICS = [
   "news",
@@ -14,52 +13,43 @@ const TOPICS = [
   "nutrition"
 ]
 
-const Home = () => {
-  const [subreddits, setSubreddits] = useState([])
-
-  // API call to get posts
-  const getPosts = useCallback(async () => {
-    for (const topic of TOPICS) {
-      await fetch(`/api/reddit/posts`, {
-        method: "POST",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          subreddit: topic,
-          filter: 'hot',
-          limit: 20
-        }),
-      }).then(response => response.json())
-        .then(data => setSubreddits(prevSubreddits => ({ ...prevSubreddits, [topic]: data.items })))
+export async function getRedditPosts({
+  subreddit,
+  filter = 'hot',
+  limit = 20,
+}) {
+  const res = await fetch(
+    `https://www.reddit.com/r/${subreddit}/${filter}.json?limit=${limit}`,
+    {
+      next: { revalidate: 300 }, // cache for 5 minutes
     }
-  }, [])
+  )
 
-  useEffect(() => {
-    getPosts()
-  }, [getPosts])
+  const json = await res.json()
+
+  return json.data.children.map((item) => item.data)
+}
+
+export default async function Home() {
+  const results = await Promise.all(
+    TOPICS.map(async topic => {
+      const items = await getRedditPosts({
+        subreddit: topic,
+        filter: 'hot',
+        limit: 20,
+      })
+
+      return [topic, items]
+    })
+  )
+
+  const subreddits = Object.fromEntries(results)
 
   return (
     <>
       <ScrollToTopOnLoad />
       <div className="px-5 md:px-8">
-        <ul className="hidden lg:flex sticky top-[85px] bg-white dark:bg-bg-dark flex space-x-10 overflow-x-auto text-lg font-medium capitalize justify-center cursor-pointer py-4 border-b border-[#efefef] dark:border-border-dark">
-          {TOPICS.map(topic => (
-            <li
-              key={"key-" + topic}
-              className="topic"
-              onClick={() => {
-                const element = document.getElementById(topic);
-                const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
-                const offsetPosition = elementPosition - 180;
-                window.scrollTo({
-                  top: offsetPosition,
-                  behavior: 'smooth'
-                });
-              }}
-            >
-              {topic}
-            </li>
-          ))}
-        </ul>
+        <TopicsBar topics={TOPICS} />
 
         <div>
           {Object.keys(subreddits)?.map(key => (
@@ -78,5 +68,3 @@ const Home = () => {
     </>
   )
 }
-
-export default Home
---
+++ src/views/Home/components/TopicsBar.jsx
@@ -0,0 +1,28 @@
+'use client'
+
+import React from "react"
+
+const TopicsBar = ({ topics }) => (
+  <ul className="hidden lg:flex sticky top-[85px] bg-white dark:bg-bg-dark flex space-x-10 overflow-x-auto text-lg font-medium capitalize justify-center cursor-pointer py-4 border-b border-[#efefef] dark:border-border-dark">
+    {topics.map(topic => (
+      <li
+        key={"key-" + topic}
+        className="topic"
+        onClick={() => {
+          const element = document.getElementById(topic);
+          const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
+          const offsetPosition = elementPosition - 180;
+          window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+          });
+        }}
+      >
+        {topic}
+      </li>
+    ))}
+  </ul>
+)
+
+export default TopicsBar
+
